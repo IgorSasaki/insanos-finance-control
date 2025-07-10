cdafb31c62aab727056bf90f928d742449f61ade
IgorSasaki <igor-sasaki@hotmail.com>
2025-07-10 10:50:20 -0300

    refactor(app): adjust pages and folders

---
+++ src/app/(public)/acesso/page.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from 'next'
+
+const Page: NextPage = () => {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-gradient-to-br from-zinc-900 via-zinc-800 to-zinc-900 p-4">
+      <h1>Helllo World</h1>
+    </main>
+  )
+}
+
+export default Page
