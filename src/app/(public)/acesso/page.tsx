3b38be8b3f6529649105d113a106a18c2ad3f567
IgorSasaki <igor-sasaki@hotmail.com>
2025-07-10 11:09:12 -0300

    feat(app acesso): implement acesso page

--- src/app/(public)/acesso/page.tsx
+++ src/app/(public)/acesso/page.tsx
@@ -1,9 +1,11 @@
 import { NextPage } from 'next'
 
+import { Container } from './_components/Container'
+
 const Page: NextPage = () => {
   return (
     <main className="flex min-h-screen items-center justify-center bg-gradient-to-br from-zinc-900 via-zinc-800 to-zinc-900 p-4">
-      <h1>Helllo World</h1>
+      <Container />
     </main>
   )
 }
