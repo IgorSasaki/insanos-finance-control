618441a1434a8d80efacdb32808c4528c0c1173e
IgorSasaki <igor-sasaki@hotmail.com>
2025-07-14 11:26:33 -0300

    feat(app (logged) layout): implement logged layout

---
+++ src/app/(logged)/layout.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import { NextPage } from 'next'
+import { PropsWithChildren } from 'react'
+
+import { Sidebar } from '@/components/structure/Sidebar'
+
+const LoggedLayout: NextPage<PropsWithChildren> = ({ children }) => {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-zinc-900 via-zinc-800 to-zinc-900">
+      <div className="flex">
+        <Sidebar />
+
+        <main className="flex-1 md:ml-64">{children}</main>
+      </div>
+    </div>
+  )
+}
+
+export default LoggedLayout
