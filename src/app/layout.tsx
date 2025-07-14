59d05586902cb87fbd9222ebae6e94d6878f1ef3
IgorSasaki <igor-sasaki@hotmail.com>
2025-07-14 11:25:58 -0300

    feat(app layout): add meta data of project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,13 @@
-import { NextPage } from 'next'
+import { Metadata, NextPage } from 'next'
 import { PropsWithChildren } from 'react'
 import { Toaster } from 'sonner'
 
 import '@/styles/globals.css'
+
+export const metadata: Metadata = {
+  title: 'Insanos MC - Sistema de Fluxo de Caixa',
+  description: 'Sistema interno de gestão financeira do Insanos MC'
+}
 
 const RootLayout: NextPage<PropsWithChildren> = async ({ children }) => {
   return (
